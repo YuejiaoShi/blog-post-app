082323c202d9bd1bc848f1c0d30d898cd3d09ffb
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 22:00:40 +0200

    Return JSON data as unknown type

--- src/util/fetchUtils.ts
+++ src/util/fetchUtils.ts
@@ -4,6 +4,8 @@ export async function get(url: string) {
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
+    const data = res.json() as unknown;
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
