38b17460c89e2d48dfa4ad91327b8f2d9ab88e34
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 22:09:36 +0200

    wrap get() with a useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { get } from "./util/fetchUtils";
 import { BlogPost } from "./components/BlogPosts";
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
-  get("https://jsonplaceholder.typicode.com/posts");
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await get("https://jsonplaceholder.typicode.com/posts");
+    }
+  }, []);
 
   return <h1>Data Fetching!</h1>;
 }
