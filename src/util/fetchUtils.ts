eea149de5bff3a570aff51d764071fc51e63aa74
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 22:02:34 +0200

    add comments

--- src/util/fetchUtils.ts
+++ src/util/fetchUtils.ts
@@ -4,8 +4,10 @@ export async function get(url: string) {
     if (!res.ok) {
       throw new Error(`HTTP error! Status: ${res.status}`);
     }
+
     const data = res.json() as unknown;
     return data;
+    // could also use 3-party (like Zod) to check data structure
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
