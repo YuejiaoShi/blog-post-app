d71788a154ad5721f4df1b81b43ce1d1b33bac6f
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-02 22:06:57 +0200

    add a state to handle fetchedPosts and return a list of BlogPost

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { get } from "./util/fetchUtils";
+import { BlogPost } from "./components/BlogPosts";
 
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>();
   get("https://jsonplaceholder.typicode.com/posts");
 
   return <h1>Data Fetching!</h1>;
